a0bd98a8ddd83ff43b1274650380de44a6bda3b6
Cameron Wright <fathobbitdotcom@gmail.com>
2012-08-10 10:41:20 +0100

    Added cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+has_many :line_items
+
+before_destroy :ensure_not_referenced_by_any_line_item
 
 validates :title, :description, :image_url, presence: true
 
@@ -14,4 +17,18 @@ validates :image_url, allow_blank: true, format: {
 	}
 
 attr_accessible :description, :image_url, :price, :title
+
+private
+
+#ensure that there are no line_items referencing this product
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, "Line items present")
+      return false
+    end
+  end
 end
+
